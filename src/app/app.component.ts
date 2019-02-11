5c54c7e421aae5dbf81c0e56e33f29b39e2ee016
stephanie williams <swilliamslmt@me.com>
2019-02-10 23:37:01 -0600

    success/warning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
+  // styleUrls: ['./app.component.css']
+  styles: [`
+  h3{
+    color:dodgerblue;
+  }
+  h4{
+    color:yellowgreen;
+  }
+  `]
+})  // you can either have the styleURLS as an array for bigger projects or
+ // you can have specified listed styles: h3 that will override the style urls.
 export class AppComponent {
-  title = 'my-first-app';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { SuccessAlertComponent } from './SuccessAlert/SuccessAlert.component';
+import { WarningalertComponent } from './warningalert/warningalert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    SuccessAlertComponent,
+    WarningalertComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
